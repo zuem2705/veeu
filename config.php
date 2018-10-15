ceb784c4a2d160abe3963127b08152628057224a
zulham2705 <42695171+zulham2705@users.noreply.github.com>
2018-10-16 00:01:39 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 // jangan hapus tanda kutip(" ") 
 
 $access_token = "43d53a7a-35ed-46db-bb12-9d40972dfcd7";
-$access_token = "da7ca230-7145-4f94-9d83-9c53f88c6a2f";
 
+<?php
+$access_token = "da7ca230-7145-4f94-9d83-9c53f88c6a2f";
 // kamu bisa membuat banyak config untuk beberapa akun.
