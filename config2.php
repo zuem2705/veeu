40254f5475af9549193f0ca718bdb684f4b19d74
zulham2705 <42695171+zulham2705@users.noreply.github.com>
2018-10-16 00:15:32 +0700

    Create config2.php

---
+++ config2.php
@@ -0,0 +1,5 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+$access_token = "43d53a7a-35ed-46db-bb12-9d40972dfcd7";
+// kamu bisa membuat banyak config untuk beberapa akun.
