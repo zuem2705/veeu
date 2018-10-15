5b4376d859065d8685c8bedc55f02008964c8428
zulham2705 <42695171+zulham2705@users.noreply.github.com>
2018-10-16 00:03:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,6 @@
 //masukan config token yg terdapat pada link di packet captur
 // jangan hapus tanda kutip(" ") 
 
-$access_token = "43d53a7a-35ed-46db-bb12-9d40972dfcd7";
+$access_token = "43d53a7a-35ed-46db-bb12-9d40972dfcd7"'da7ca230-7145-4f94-9d83-9c53f88c6a2f";
 
-<?php
-$access_token = "da7ca230-7145-4f94-9d83-9c53f88c6a2f";
 // kamu bisa membuat banyak config untuk beberapa akun.
