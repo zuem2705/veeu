dc1094f786b0dc882be443a30b63e1056ba027ed
zulham2705 <42695171+zulham2705@users.noreply.github.com>
2018-10-15 23:24:33 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = "a1b5f3aa-7144-47c2-a402-b7797b5e9a97";
+
+// kamu bisa membuat banyak config untuk beberapa akun.
